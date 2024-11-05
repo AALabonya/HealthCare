befe5bc0a84b253328e5e55d5a43b469a1248357
AALabonya <1aklimakter@gmail.com>
2024-11-05 19:33:05 +0600

    create controllers

--- src/app/modules/User/user.controller.ts
+++ src/app/modules/User/user.controller.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from "express";
 import { userService } from "./user.service";
+import { log } from "console";
 
 const createAdmin = async(req:Request, res:Response)=>{
-  const result = await  userService.createAdmin();
+    // console.log(req.body)
+  const result = await  userService.createAdmin(req.body);
   res.send(result)
 }
 
