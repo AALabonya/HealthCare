2eaf6acab4e972093291432bc624fe167a76d271
AALabonya <1aklimakter@gmail.com>
2024-11-08 00:14:31 +0600

    create controllers

--- src/app/modules/Admin/admin.service.ts
+++ src/app/modules/Admin/admin.service.ts
@@ -3,4 +3,9 @@ import { PrismaClient } from "@prisma/client"
 const prisma =new PrismaClient()
 const getAllFromDB= async()=>{
     const result = await prisma.admin.findMany()
+    return result
+}
+
+export const adminServices={
+    getAllFromDB
 }
