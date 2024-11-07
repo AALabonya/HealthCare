d1bdb6d7b6b337fca5feb6e862da826f6203ff53
AALabonya <1aklimakter@gmail.com>
2024-11-07 23:45:25 +0600

    create controllers

--- src/app/modules/User/user.controller.ts
+++ src/app/modules/User/user.controller.ts
@@ -5,7 +5,12 @@ import { log } from "console";
 const createAdmin = async(req:Request, res:Response)=>{
     // console.log(req.body)
   const result = await  userService.createAdmin(req.body);
-  res.send(result)
+  // res.send(result)
+  res.status(200).json({
+    success:true,
+    message:"Admin Created successfully",
+    data:result
+  })
 }
 
 export const userController ={
