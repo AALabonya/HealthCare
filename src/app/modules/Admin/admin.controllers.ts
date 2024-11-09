9e868b924007292a7ff668d21dad47a3996186a7
AALabonya <1aklimakter@gmail.com>
2024-11-09 10:41:54 +0600

    added searchTerm

--- src/app/modules/Admin/admin.controllers.ts
+++ src/app/modules/Admin/admin.controllers.ts
@@ -2,12 +2,13 @@
 import { Request, Response } from "express";
 import { adminServices } from "./admin.service";
 import pick from "../../../shared/pick";
+import { adminFilterableFields } from "./admin.constant";
 
 
 
 const getAllFromDB= async(req:Request, res:Response)=>{
     try {
-      const filters = pick(req.query,['name','email','searchTerm','contactNumber'])
+      const filters = pick(req.query,adminFilterableFields)
        const result = await adminServices.getAllFromDB(filters)
      res.status(200).json({
         success:true,
