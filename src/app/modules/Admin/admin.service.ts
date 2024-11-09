724b2cd774cfa5036ae60969c74e018ccd2fd830
AALabonya <1aklimakter@gmail.com>
2024-11-09 10:17:17 +0600

    create controllers

--- src/app/modules/Admin/admin.service.ts
+++ src/app/modules/Admin/admin.service.ts
@@ -1,8 +1,9 @@
 import { Prisma, PrismaClient } from "@prisma/client"
+import { object } from "zod";
 
 const prisma =new PrismaClient()
 const getAllFromDB= async(params:any)=>{
-
+const {searchTerm, ...filterData}=params
     const andConditions:Prisma.AdminWhereInput[] =[];
     // OR:[
     //     {
@@ -24,6 +25,15 @@ const adminSearchFields=['name','email'];
                 [field]:{
                     contains:params.searchTerm, 
              mode:'insensitive'}
+            }))
+        })
+    }
+    if(Object.keys(filterData).length >0){
+        andConditions.push({
+            AND:Object.keys(filterData).map(key =>({
+                [key]:{
+                    equals:filterData[key]
+                }
             }))
         })
     }
