41795ccf3c0e62131d2ecfec427fafd26374b1b0
AALabonya <1aklimakter@gmail.com>
2024-11-09 10:35:53 +0600

    added searchTerm

--- src/app/modules/Admin/admin.controllers.ts
+++ src/app/modules/Admin/admin.controllers.ts
@@ -1,8 +1,16 @@
-import { PrismaClient } from "@prisma/client";
+
 import { Request, Response } from "express";
 import { adminServices } from "./admin.service";
 
-const prisma =new PrismaClient()
+const pick =<T extends Record<string, unknown>, k extends keyof T>(obj:T, keys:k[]):Partial<T> =>{
+   const finalObj:Partial<T>={};
+   for(const key of keys){
+      if(obj && Object.hasOwnProperty.call(obj, key)){
+      finalObj[key]=obj[key]
+      }
+   }
+   return finalObj
+}
 
 const getAllFromDB= async(req:Request, res:Response)=>{
     try {
