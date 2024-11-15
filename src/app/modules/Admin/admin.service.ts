21a6b81c1c915058b671d77206a0324492fa6297
AALabonya <1aklimakter@gmail.com>
2024-11-15 21:09:26 +0600

    added validations

--- src/app/modules/Admin/admin.service.ts
+++ src/app/modules/Admin/admin.service.ts
@@ -2,8 +2,10 @@ import { Admin, Prisma, UserStatus } from "@prisma/client";
 import prisma from "../../../shared/prisma";
 import { paginationHelper } from "../../../helpers/paginationHelper";
 import { adminSearchAbleFields } from "./admin.constant";
+import { IAdminFilterRequest } from "./admin.interface";
+import { IPaginationOptions } from "../../interface/pagination";
 
-const getAllFromDB = async (params: any, options: any) => {
+const getAllFromDB = async (params:IAdminFilterRequest, options:IPaginationOptions) => {
     const { page, limit, skip } = paginationHelper.calculatePagination(options);
     const { searchTerm, ...filterData } = params;
 
@@ -25,7 +27,7 @@ const getAllFromDB = async (params: any, options: any) => {
         andCondions.push({
             AND: Object.keys(filterData).map(key => ({
                 [key]: {
-                    equals: filterData[key]
+                    equals: (filterData as any)[key]
                 }
             }))
         })
