d3271a6f72924f56dc4c54f862a62669e251251e
AALabonya <1aklimakter@gmail.com>
2024-11-15 20:31:12 +0600

    added validations

--- src/app/modules/User/user.controller.ts
+++ src/app/modules/User/user.controller.ts
@@ -15,7 +15,7 @@ const createAdmin = async (req: Request, res: Response) => {
     catch (err) {
         res.status(500).json({
             success: false,
-            message: err?.name || "Something went wrong",
+            message: err || "Something went wrong",
             error: err
         })
     }
