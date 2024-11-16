1d7f93fe1e0cd636287037921d8fdc7e5b09b685
AALabonya <1aklimakter@gmail.com>
2024-11-17 00:21:16 +0600

    added login

--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -1,9 +1,16 @@
 import { log } from "console"
+import prisma from "../../../shared/prisma";
 
 const loginUser= async(payload:{
     email:string,
     password:string
 })=>{
+    const userData = await prisma.user.findFirstOrThrow({
+        where:{
+            email:payload.email
+        }
+    })
+    
     console.log("login service", payload);
     
 }
