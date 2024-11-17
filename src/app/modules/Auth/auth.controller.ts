e4f4018debc56e690c52183b13d1c4eed537a1a4
AALabonya <1aklimakter@gmail.com>
2024-11-17 21:43:56 +0600

    added login

--- src/app/modules/Auth/auth.controller.ts
+++ src/app/modules/Auth/auth.controller.ts
@@ -4,45 +4,43 @@ import { authServices } from "./auth.service";
 import sendResponse from "../../../shared/sendResponse";
 import httpStatus from "http-status";
 
-const loginUser = catchAsync(async(req:Request, res:Response)=>{
-    const result = await authServices.loginUser(req.body)
-const{refreshToken} = result;
+const loginUser = catchAsync(async (req: Request, res: Response) => {
+    const result = await authServices.loginUser(req.body);
 
-    res.cookie('refresh',refreshToken,{
-        secure:false,
-        httpOnly:true
-    })
+    const { refreshToken } = result;
 
+    res.cookie('refreshToken', refreshToken, {
+        secure: false,
+        httpOnly: true
+    });
 
-
-    sendResponse(res,{
-        statusCode:httpStatus.OK,
-        success:true,
-        message:"logged id successfully",
-        data:{
-            accessToken:result.accessToken,
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: "Logged in successfully!",
+        data: {
+            accessToken: result.accessToken,
             needPasswordChange: result.needPasswordChange
         }
     })
-})
-const refreshToken = catchAsync(async(req:Request, res:Response)=>{
-    const {refreshToken}= req.cookies;
-    // console.log("refresh token", refreshToken);
-    
-    const result = await authServices.refreshToken(refreshToken)
+});
 
+const refreshToken = catchAsync(async (req: Request, res: Response) => {
+    const { refreshToken } = req.cookies;
 
-    sendResponse(res,{
-        statusCode:httpStatus.OK,
-        success:true,
-        message:"logged id successfully",
-        data:result
-        // data:{
-        //     accessToken:result.accessToken,
+    const result = await authServices.refreshToken(refreshToken);
+
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: "Access token genereated successfully!",
+        data: result
+        // data: {
+        //     accessToken: result.accessToken,
         //     needPasswordChange: result.needPasswordChange
         // }
     })
-})
+});
 export const authController={
     loginUser,
     refreshToken
